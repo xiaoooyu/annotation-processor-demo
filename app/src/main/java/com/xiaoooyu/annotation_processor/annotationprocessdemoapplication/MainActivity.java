14ddd9a8e79ec78d692547ff52d625ff86b3d833
Yin Xiaoyu <xiaoyu@teambition.com>
2017-05-08 18:20:20 +0800

    adding CustomAnnotationProcessor

--- app/src/main/java/com/xiaoooyu/annotation_processor/annotationprocessdemoapplication/MainActivity.java
+++ app/src/main/java/com/xiaoooyu/annotation_processor/annotationprocessdemoapplication/MainActivity.java
@@ -3,8 +3,12 @@ package com.xiaoooyu.annotation_processor.annotationprocessdemoapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.xiaoooyu.annotationprocessor.processor.CustomAnnotation;
+
+@CustomAnnotation
 public class MainActivity extends AppCompatActivity {
 
+    @CustomAnnotation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ processor/src/main/java/com/xiaoooyu/annotationprocessor/processor/CustomAnnotation.java
@@ -0,0 +1,9 @@
+package com.xiaoooyu.annotationprocessor.processor;
+
+/**
+ * Copyright (c) 2017 Xiaoooyu All Rights Reserved.
+ */
+public @interface CustomAnnotation {
+
+
+}
