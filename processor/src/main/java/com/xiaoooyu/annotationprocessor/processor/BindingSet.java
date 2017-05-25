2b23aa5efe7e72839efbe404410e2f809753a08e
Yin Xiaoyu <xiaoyu@teambition.com>
2017-05-25 17:21:34 +0800

    adding R.id.xxx to replace int

--- processor/src/main/java/com/xiaoooyu/annotationprocessor/processor/BindingSet.java
+++ processor/src/main/java/com/xiaoooyu/annotationprocessor/processor/BindingSet.java
@@ -1,6 +1,7 @@
 package com.xiaoooyu.annotationprocessor.processor;
 
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
@@ -30,7 +31,7 @@ public class BindingSet {
     protected TypeElement mEnclosingElement;
 
     // mapping from Id to executable method
-    protected Map<Integer, TraceMethod> mIdToTraceMethod;
+    protected Map<Id, TraceMethod> mIdToTraceMethod;
 
     protected ClassName mBindingClassName;
     protected ClassName mTargetClassName;
@@ -54,10 +55,9 @@ public class BindingSet {
      * @param traceInfo
      * @return ture if success
      */
-    public boolean addBinding(int id, ExecutableElement bindingElement, TraceClick traceInfo) {
-        Integer key = new Integer(id);
-        if (!mIdToTraceMethod.containsKey(key)) {
-            mIdToTraceMethod.put(key, new TraceMethod(traceInfo, bindingElement));
+    public boolean addBinding(Id id, ExecutableElement bindingElement, TraceClick traceInfo) {
+        if (!mIdToTraceMethod.containsKey(id)) {
+            mIdToTraceMethod.put(id, new TraceMethod(traceInfo, bindingElement));
             return true;
         }
         return false;
@@ -89,22 +89,50 @@ public class BindingSet {
         result.addField(targetTypeVariableName, "mTarget", PRIVATE);
         result.addField(VIEW, "mView", PRIVATE);
 
-        result.addMethod(createContructor(targetTypeVariableName));
+        result.addMethod(createConstructor(targetTypeVariableName));
 
         return result.build();
     }
 
-    private MethodSpec createContructor(TypeName targetType) {
+    private MethodSpec createConstructor(TypeName targetType) {
         MethodSpec.Builder constructor = MethodSpec.constructorBuilder()
             .addAnnotation(UI_THREAD)
             .addModifiers(PUBLIC);
 
-        constructor.addParameter(targetType, "target");
-        constructor.addParameter(VIEW, "source");
+        ClassName clickListener = ClassName.get("butterknife.internal", "DebouncingOnClickListener");
 
+        constructor.addParameter(targetType, "target");
+        constructor.addParameter(VIEW, "view");
         constructor.addStatement("mTarget = target");
-        constructor.addStatement("mView = source");
+
+        for (Map.Entry<Id, TraceMethod> entry : mIdToTraceMethod.entrySet()) {
+            Id id = entry.getKey();
+            TraceMethod method = entry.getValue();
+            constructor.addCode("view.findViewById($L).setOnClickListener(new $T() {\n" +
+                "      @Override\n" +
+                "      public void doClick(View p0) {\n", CodeBlock.of("$L", id.code), clickListener);
+//            addTraceInfoStatement(constructor, method.mTraceInfo);
+
+            String invokeMethod;
+            if (method.mExecutableElement.getParameters().size() > 0) {
+                invokeMethod = "\tmTarget.$L(p0);\n";
+            } else {
+                invokeMethod = "\tmTarget.$L();\n";
+            }
+
+            constructor.addStatement(invokeMethod +
+                "      }})", method.mExecutableElement.getSimpleName());
+        }
 
         return constructor.build();
+    }
+
+    private void addTraceInfoStatement(MethodSpec.Builder builder, TraceClick traceInfo) {
+        ClassName analyticsUtil = ClassName.get("com.teambition.teambition.util", "AnalyticsUtil");
+        ClassName r = ClassName.get("com.teambition.teambition", "R");
+
+        builder.addStatement("$1T.EventBuilder builder = $1T.make()", analyticsUtil)
+            .addStatement("builder.putProps($T.string.a_eprop_page, $L)", r, traceInfo.page())
+            .addStatement("builder.trackEvent($L)", traceInfo.event());
     }
 }
--- processor/src/main/java/com/xiaoooyu/annotationprocessor/processor/CustomAnnotationProcessor.java
+++ processor/src/main/java/com/xiaoooyu/annotationprocessor/processor/CustomAnnotationProcessor.java
@@ -2,14 +2,21 @@ package com.xiaoooyu.annotationprocessor.processor;
 
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.util.Trees;
+import com.sun.tools.javac.code.Symbol;
+import com.sun.tools.javac.tree.JCTree;
+import com.sun.tools.javac.tree.TreeScanner;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.io.Writer;
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -20,31 +27,53 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.TypeKind;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.MirroredTypeException;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
-import javax.tools.JavaFileObject;
 
 @SupportedAnnotationTypes(value = {"com.xiaoooyu.annotationprocessor.processor.CustomAnnotation",
     "com.xiaoooyu.annotationprocessor.processor.TrackClick"})
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class CustomAnnotationProcessor extends AbstractProcessor {
 
+    private static final List<String> SUPPORTED_TYPES = Arrays.asList(
+        "array", "attr", "bool", "color", "dimen", "drawable", "id", "integer", "string"
+    );
+
     private Filer mFiler;
+    private Elements mElementUtils;
+    private Trees mTrees;
+    private Types mTypeUtils;
+
+    private final Map<QualifiedId, Id> mSymbols = new LinkedHashMap<>();
 
     @Override
     public synchronized void init(ProcessingEnvironment env) {
         super.init(env);
 
         mFiler = env.getFiler();
+        mElementUtils = env.getElementUtils();
+        mTypeUtils = env.getTypeUtils();
+
+        try {
+            mTrees = Trees.instance(env);
+        } catch (IllegalArgumentException ignored) {
+
+        }
     }
 
     @Override public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
+
+        scanForRClasses(env);
+
         Map<TypeElement, BindingSet> traceMap = new HashMap<>();
 
         for (Element element: env.getElementsAnnotatedWith(TraceClick.class)) {
@@ -74,7 +103,7 @@ public class CustomAnnotationProcessor extends AbstractProcessor {
 
         Name qualifiedName = enclosingElement.getQualifiedName();
 
-        note(element, "enclosingElement is %s", qualifiedName);
+//        note(element, "enclosingElement is %s", qualifiedName);
 
         if (!buildMap.containsKey(enclosingElement)) {
             BindingSet temp = new BindingSet(enclosingElement);
@@ -92,8 +121,21 @@ public class CustomAnnotationProcessor extends AbstractProcessor {
             executableElement = (ExecutableElement) element;
         }
 
-        Integer viewId = traceInfo.id();
-        set.addBinding(viewId, executableElement, traceInfo);
+        int viewId = traceInfo.id();
+        QualifiedId qualifiedId = elementToQualifiedId(element, viewId);
+
+        set.addBinding(getId(qualifiedId), executableElement, traceInfo);
+    }
+
+    private QualifiedId elementToQualifiedId(Element element, int id) {
+        return new QualifiedId(mElementUtils.getPackageOf(element).getQualifiedName().toString(), id);
+    }
+
+    private Id getId(QualifiedId qualifiedId) {
+        if (mSymbols.get(qualifiedId) == null) {
+            mSymbols.put(qualifiedId, new Id(qualifiedId.id));
+        }
+        return mSymbols.get(qualifiedId);
     }
 
     private void error(Element element, String message, Object... args) {
@@ -119,4 +161,166 @@ public class CustomAnnotationProcessor extends AbstractProcessor {
         error(element, "Unable to parse @%s binding.\n\n%s", annotation.getSimpleName(), stackTrace);
     }
 
+    private void scanForRClasses(RoundEnvironment env) {
+        if (mTrees == null) return;
+
+        RClassScanner scanner = new RClassScanner();
+
+        for (Class<? extends Annotation> annotation : getSupportedAnnotations()) {
+            for (Element element : env.getElementsAnnotatedWith(annotation)) {
+                JCTree tree = (JCTree) mTrees.getTree(element, getMirror(element, annotation));
+                if (tree != null) { // tree can be null if the references are compiled types and not source
+                    String respectivePackageName =
+                        mElementUtils.getPackageOf(element).getQualifiedName().toString();
+                    scanner.setCurrentPackageName(respectivePackageName);
+                    tree.accept(scanner);
+                }
+            }
+        }
+
+        for (Map.Entry<String, Set<String>> packageNameToRClassSet : scanner.getRClasses().entrySet()) {
+            String respectivePackageName = packageNameToRClassSet.getKey();
+            for (String rClass : packageNameToRClassSet.getValue()) {
+                parseRClass(respectivePackageName, rClass);
+            }
+        }
+    }
+
+    private void parseRClass(String respectivePackageName, String rClass) {
+        Element element;
+
+        try {
+            element = mElementUtils.getTypeElement(rClass);
+        } catch (MirroredTypeException mte) {
+            element = mTypeUtils.asElement(mte.getTypeMirror());
+        }
+
+        JCTree tree = (JCTree) mTrees.getTree(element);
+        if (tree != null) { // tree can be null if the references are compiled types and not source
+            IdScanner idScanner = new IdScanner(mSymbols, mElementUtils.getPackageOf(element)
+                .getQualifiedName().toString(), respectivePackageName);
+            tree.accept(idScanner);
+        } else {
+            parseCompiledR(respectivePackageName, (TypeElement) element);
+        }
+    }
+
+    private void parseCompiledR(String respectivePackageName, TypeElement rClass) {
+        for (Element element : rClass.getEnclosedElements()) {
+            String innerClassName = element.getSimpleName().toString();
+            if (SUPPORTED_TYPES.contains(innerClassName)) {
+                for (Element enclosedElement : element.getEnclosedElements()) {
+                    if (enclosedElement instanceof VariableElement) {
+                        VariableElement variableElement = (VariableElement) enclosedElement;
+                        Object value = variableElement.getConstantValue();
+
+                        if (value instanceof Integer) {
+                            int id = (Integer) value;
+                            ClassName rClassName =
+                                ClassName.get(mElementUtils.getPackageOf(variableElement).toString(), "R",
+                                    innerClassName);
+                            String resourceName = variableElement.getSimpleName().toString();
+                            QualifiedId qualifiedId = new QualifiedId(respectivePackageName, id);
+                            mSymbols.put(qualifiedId, new Id(id, rClassName, resourceName));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private Set<Class<? extends Annotation>> getSupportedAnnotations() {
+        Set<Class<? extends Annotation>> annotations = new LinkedHashSet<>();
+        annotations.add(TraceClick.class);
+
+        return annotations;
+    }
+
+    private static AnnotationMirror getMirror(Element element,
+                                              Class<? extends Annotation> annotation) {
+        for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
+            if (annotationMirror.getAnnotationType().toString().equals(annotation.getCanonicalName())) {
+                return annotationMirror;
+            }
+        }
+        return null;
+    }
+
+
+    private static class RClassScanner extends TreeScanner {
+        // Maps the currently evaulated rPackageName to R Classes
+        private final Map<String, Set<String>> rClasses = new LinkedHashMap<>();
+        private String currentPackageName;
+
+        @Override public void visitSelect(JCTree.JCFieldAccess jcFieldAccess) {
+            Symbol symbol = jcFieldAccess.sym;
+            if (symbol != null
+                && symbol.getEnclosingElement() != null
+                && symbol.getEnclosingElement().getEnclosingElement() != null
+                && symbol.getEnclosingElement().getEnclosingElement().enclClass() != null) {
+                Set<String> rClassSet = rClasses.get(currentPackageName);
+                if (rClassSet == null) {
+                    rClassSet = new HashSet<>();
+                    rClasses.put(currentPackageName, rClassSet);
+                }
+                rClassSet.add(symbol.getEnclosingElement().getEnclosingElement().enclClass().className());
+            }
+        }
+
+        Map<String, Set<String>> getRClasses() {
+            return rClasses;
+        }
+
+        void setCurrentPackageName(String respectivePackageName) {
+            this.currentPackageName = respectivePackageName;
+        }
+    }
+
+    private static class IdScanner extends TreeScanner {
+        private final Map<QualifiedId, Id> ids;
+        private final String rPackageName;
+        private final String respectivePackageName;
+
+        IdScanner(Map<QualifiedId, Id> ids, String rPackageName, String respectivePackageName) {
+            this.ids = ids;
+            this.rPackageName = rPackageName;
+            this.respectivePackageName = respectivePackageName;
+        }
+
+        @Override public void visitClassDef(JCTree.JCClassDecl jcClassDecl) {
+            for (JCTree tree : jcClassDecl.defs) {
+                if (tree instanceof ClassTree) {
+                    ClassTree classTree = (ClassTree) tree;
+                    String className = classTree.getSimpleName().toString();
+                    if (SUPPORTED_TYPES.contains(className)) {
+                        ClassName rClassName = ClassName.get(rPackageName, "R", className);
+                        VarScanner scanner = new VarScanner(ids, rClassName, respectivePackageName);
+                        ((JCTree) classTree).accept(scanner);
+                    }
+                }
+            }
+        }
+    }
+
+    private static class VarScanner extends TreeScanner {
+        private final Map<QualifiedId, Id> ids;
+        private final ClassName className;
+        private final String respectivePackageName;
+
+        private VarScanner(Map<QualifiedId, Id> ids, ClassName className,
+                           String respectivePackageName) {
+            this.ids = ids;
+            this.className = className;
+            this.respectivePackageName = respectivePackageName;
+        }
+
+        @Override public void visitVarDef(JCTree.JCVariableDecl jcVariableDecl) {
+            if ("int".equals(jcVariableDecl.getType().toString())) {
+                int id = Integer.valueOf(jcVariableDecl.getInitializer().toString());
+                String resourceName = jcVariableDecl.getName().toString();
+                QualifiedId qualifiedId = new QualifiedId(respectivePackageName, id);
+                ids.put(qualifiedId, new Id(id, className, resourceName));
+            }
+        }
+    }
 }
